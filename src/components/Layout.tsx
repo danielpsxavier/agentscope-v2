624e68bafc469fba7e20662f8851ddf1430b06c0
Skip <skip@usecurling.com>
2025-11-05 00:49:02 +0000

    Remove flex class from main layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,19 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import { Sidebar } from '@/components/Sidebar'
 import { SidebarTrigger } from '@/components/ui/sidebar'
 import { Button } from '@/components/ui/button'
 import { Menu } from 'lucide-react'
+import { cn } from '@/lib/utils'
 
 export default function Layout() {
+  const location = useLocation()
+  const isHomePage = location.pathname === '/'
+
   return (
     <div className="flex min-h-screen bg-neutral-pageBackground">
       <Sidebar />
       <div className="flex-1 flex flex-col min-w-0">
-        <main className="flex-1 flex">
+        <main className={cn('flex-1', { flex: !isHomePage })}>
           <div className="lg:hidden fixed top-4 left-4 z-50">
             <SidebarTrigger asChild>
               <Button
